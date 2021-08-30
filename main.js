ad13c826eca9eb57b08b1865151d83378d43a742
Wizard2D <70329712+Wizard2D@users.noreply.github.com>
2021-08-30 21:01:29 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 function onClick(){
-  window.location.href = concat("wizard2d.github.io/2h2d/",toString(document.GetElementById("CCN").innerText),".html");
+  window.location.href = "wizard2d.github.io/2h2d/"+toString(document.GetElementById("CCN").innerText)+".html";
 }
